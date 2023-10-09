f7bd128d3997a0b5c670c65c70f8eb512cae0e79
peninah98 <peninangizwenayo@gmail.com>
2023-10-09 20:10:48 +0200

    Password validation updates

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -1,19 +1,17 @@
 export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 
-/**
- * Checks if a given password is valid or invalid.
- * If valid it returns true, otherwise false
- * @param {string} password
- * @returns {boolean}
- */
-export default function isValidPassword(password = "") {
-  if(password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d){10}$/gm) ){
-    return true
+export default function isValidPassword(password) {
+  if (typeof password !== 'string') password = String(password.sort((a,b)=>a-b));
+   const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{10}$/;
+  if(regex.test(password) && !forbiddenPasswords.includes(password) ) {
+    if(new Set([...password]).size > 3) return true
   }
-  else{
-    return false
-  }
-  
+    return false 
+   
 }
+
+
+
+
 
 
