c71232bc1e0de0f8358db5a7e53b8659e8283cd4
peninah98 <peninangizwenayo@gmail.com>
2023-10-16 14:24:59 +0200

    Updates

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -1,23 +1,21 @@
 export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 
- const isValidPassword =(password)=> {
-  if (!typeof password === 'string') {
-   return String(password.sort((a,b)=>a-b))
-  } 
+const isValidPassword = (password) => {
+  if (!typeof password === "string") {
+    return String(password.sort((a, b) => a - b))
+  }
 
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{10}$/;
-  if(forbiddenPasswords.includes(password)) return false
-  if(!regex.test(password)) return false
-  if(!new Set([...password]).size > 3) return false
-  if(password<4) return false
-  
-   
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{10}$/
+  if (forbiddenPasswords.includes(password)) return false
+  if (!regex.test(password)) return false
+  if (!new Set([...password]).size > 3) return false
+  if (password < 4) return false
+  if (
+    ![...password].every(
+      (char, index, array) => index === 0 || char >= array[index - 1]
+    )
+  )
+    return false
 }
 
 export default isValidPassword
-
-
-
-
-
-
