c255179d207fa6a2f3514b5a31980718719c7a22
peninah98 <peninangizwenayo@gmail.com>
2023-10-17 10:55:44 +0200

    feat: password penality

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -7,12 +7,12 @@
  */
 
 
-export default function penaltyPoints(password = "") {
+const penaltyPoints = (password = "") => {
 if(password === null) return 0;  
 if (typeof password !== "string") password = String(password);
   let count =0
-  const pattern = /([a-z0-9])\1+/gi;
-  const matches = password.match(pattern) || 0;
+  const regex = /([a-z0-9])\1+/gi;
+  const matches = password.match(regex) || 0;
   if(matches === 0) return 0
   for(let i = 0; i<matches.length;i++){
     if(matches[i].length === 2){
@@ -26,3 +26,6 @@ if (typeof password !== "string") password = String(password);
   return count || 0
 
 }
+
+
+export default penaltyPoints
