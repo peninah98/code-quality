7967449cbee679d5a753ea07d2fe87c458506d89
peninah98 <peninangizwenayo@gmail.com>
2023-10-19 10:52:59 +0200

    fix: removing the comments in the codes

--- src/improvement-3.js
+++ src/improvement-3.js
@@ -1,10 +1,3 @@
-/**
- *  TRY TO IMPROVE THE FOLLOWING FUNCTION.
- *  YOU CAN REPLACE THE CODE INSIDE THE FUNCTIONS, THE FUNCTION NAME, ETC.
- *  PLEASE DO NOT REMOVE `export default function`
- */
-
-// FUNCTION 3
 const largestNumber = numbers => Math.max(...numbers)
 
 export default largestNumber
