b63a6eee80248ff48cf85af5383f8f672f24904c
peninah98 <peninangizwenayo@gmail.com>
2023-10-19 10:51:45 +0200

    fix: removing comments

--- src/improvement-2.js
+++ src/improvement-2.js
@@ -1,10 +1,3 @@
-/**
- *  TRY TO IMPROVE THE FOLLOWING FUNCTION.
- *  YOU CAN REPLACE THE CODE INSIDE THE FUNCTIONS, THE FUNCTION NAME, ETC.
- *  PLEASE DO NOT REMOVE `export default function`
- */
-
-// FUNCTION 2
 const food = items => items.filter((item)=> item.type === 'Food')
 
 export default food
