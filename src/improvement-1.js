8f144c7fd0a88e442201aa854937b89afd946336
peninah98 <peninangizwenayo@gmail.com>
2023-10-19 10:49:56 +0200

    fix: removing comments

--- src/improvement-1.js
+++ src/improvement-1.js
@@ -1,10 +1,3 @@
-/**
- *  TRY TO IMPROVE THE FOLLOWING FUNCTION.
- *  YOU CAN REPLACE THE CODE INSIDE THE FUNCTIONS, THE FUNCTION NAME, ETC.
- *  PLEASE DO NOT REMOVE `export default function`
- */
-
-// FUNCTION 1
 export default function countingRs(string) {
  return string.split("").filter(element=>element=== "R").length
   
