80ef6f3032f50b78f221a7d84f6310a3d72d4d71
peninah98 <peninangizwenayo@gmail.com>
2023-10-19 10:45:42 +0200

    fix: removing the comments in the codes

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -1,11 +1,3 @@
-/**
- * Calculates the sum of penalty points for a given password.
- * Double characters like `aa` count as 1 penalty point, triples and more are 2 points.
- * It returns the sum of penalty points for a password or 0.
- * @param {string} password
- * @returns {number}
- */
-
 const penaltyPoints = (password = "") => {
   if (password === null) return 0
   if (typeof password !== "string") password = String(password)
